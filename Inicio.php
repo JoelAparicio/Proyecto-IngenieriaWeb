6e52cdb9507f04fbf9809698018f171803c66cd5
JoelAparicio <105009975+JoelAparicio@users.noreply.github.com>
2022-11-26 15:27:59 -0500

    Desarrollo de inicio 1

---
+++ Inicio.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+    <link href="index.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+    <header>
+        <div id="encabezado-logo">
+            <img scr="Imagenes/inicio/logo.png">
+        </div>
+        <nav>
+
+        </nav>
+    </header>
+
+    <div class="carrusel-noticias">
+
+        <div id="card-noticia">
+            <a href="https://www.marca.com/futbol/mundial/2022/11/25/6381284122601dc57c8b45e9.html"><h3>Noticia 1</h3></a>
+            <p>Inglaterra no concretó su pase a octavos y Estados Unidos sumó un empate que lo deja complicado en el Grupo B.</p>
+            <img src="https://phantom-marca.unidadeditorial.es/1eabf9af319ccd8314e9533ed32ffb75/resize/1320/f/webp/assets/multimedia/imagenes/2022/11/25/16694087916622.jpg" alt="imagen" id="imagen-noticia">
+        </div>
+        <div id="card-noticia">
+            <a href="https://www.marca.com/futbol/mundial/2022/11/25/6381284122601dc57c8b45e9.html"><h3>Noticia 1</h3></a>
+            <p>Inglaterra no concretó su pase a octavos y Estados Unidos sumó un empate que lo deja complicado en el Grupo B.</p>
+            <img src="https://phantom-marca.unidadeditorial.es/1eabf9af319ccd8314e9533ed32ffb75/resize/1320/f/webp/assets/multimedia/imagenes/2022/11/25/16694087916622.jpg" alt="imagen" id="imagen-noticia">
+        </div>
+        <div id="card-noticia">
+            <a href="https://www.marca.com/futbol/mundial/2022/11/25/6381284122601dc57c8b45e9.html"><h3>Noticia 1</h3></a>
+            <p>Inglaterra no concretó su pase a octavos y Estados Unidos sumó un empate que lo deja complicado en el Grupo B.</p>
+            <img src="https://phantom-marca.unidadeditorial.es/1eabf9af319ccd8314e9533ed32ffb75/resize/1320/f/webp/assets/multimedia/imagenes/2022/11/25/16694087916622.jpg" alt="imagen" id="imagen-noticia">
+        </div>
+        <div id="card-noticia">
+            <a href="https://www.marca.com/futbol/mundial/2022/11/25/6381284122601dc57c8b45e9.html"><h3>Noticia 1</h3></a>
+            <p>Inglaterra no concretó su pase a octavos y Estados Unidos sumó un empate que lo deja complicado en el Grupo B.</p>
+            <img src="https://phantom-marca.unidadeditorial.es/1eabf9af319ccd8314e9533ed32ffb75/resize/1320/f/webp/assets/multimedia/imagenes/2022/11/25/16694087916622.jpg" alt="imagen" id="imagen-noticia">
+        </div>
+        <div id="card-noticia">
+            <a href="https://www.marca.com/futbol/mundial/2022/11/25/6381284122601dc57c8b45e9.html"><h3>Noticia 1</h3></a>
+            <p>Inglaterra no concretó su pase a octavos y Estados Unidos sumó un empate que lo deja complicado en el Grupo B.</p>
+            <img src="https://phantom-marca.unidadeditorial.es/1eabf9af319ccd8314e9533ed32ffb75/resize/1320/f/webp/assets/multimedia/imagenes/2022/11/25/16694087916622.jpg" alt="imagen" id="imagen-noticia">
+        </div>
+        <div id="card-noticia">
+            <a href="https://www.marca.com/futbol/mundial/2022/11/25/6381284122601dc57c8b45e9.html"><h3>Noticia 1</h3></a>
+            <p>Inglaterra no concretó su pase a octavos y Estados Unidos sumó un empate que lo deja complicado en el Grupo B.</p>
+            <img src="https://phantom-marca.unidadeditorial.es/1eabf9af319ccd8314e9533ed32ffb75/resize/1320/f/webp/assets/multimedia/imagenes/2022/11/25/16694087916622.jpg" alt="imagen" id="imagen-noticia">
+        </div>
+
+        <ul id="puntos">
+            <li id="punto"></li>
+            <li id="punto"></li>
+        </ul>
+
+    </div>
+    
+</body>
+</html>

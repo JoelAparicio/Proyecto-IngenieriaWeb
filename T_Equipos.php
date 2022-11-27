e06c527175c47f99c76dbe25fad1a38a937d50e6
airlenys21 <airlenys2130@gmail.com>
2022-11-26 23:21:21 -0500

    Paquina de equipos

--- T_Equipos.php
+++ T_Equipos.php
@@ -29,14 +29,179 @@
             </ul>
         </nav>
     </header>
-
-    <div> 
-       <h2>Equipos del Mundial</h2>
-    </div>
-
-    <div>
-
     
+    <div>
+        <div> 
+            <h1>Equipos</h1>
+        </div>
+        <div class="Continente"> 
+            <div class="Continente-mapa"> 
+                <h2>Africa</h2>
+                <img src="Imagenes/Continentes/Africa.png">
+            </div>
+            <div class="Continente-Banderas"> 
+                <div id="Bandera"> <a href="">
+                    <img src="Imagenes/Continente_Equipos/Africa/Camerún/Bandera Camerún.png">
+                    <p>Camerún</p></a>
+                </div>
+                <div id="Bandera"> <a href="">
+                    <img src="Imagenes/Continente_Equipos/Africa/Ghana/Bandera Ghana.png">
+                    <p>Ghana</p></a>
+                </div>
+                <div id="Bandera"> <a href="">
+                    <img src="Imagenes/Continente_Equipos/Africa/Marruecos/Bandera Marruecos.png">
+                    <p>Marruecos</p></a>
+                </div>
+                <div id="Bandera"> <a href="">
+                    <img src="Imagenes/Continente_Equipos/Africa/Senegal/Bandera Senegal.png">
+                    <p>Senegal</p> </a>
+                </div>
+                <div id="Bandera"> <a href="">
+                    <img src="Imagenes/Continente_Equipos/Africa/Túnez/Bandera Túnez.png">
+                    <p>Túnez</p> </a>
+                </div>
+            </div>
+        </div>
+        <hr style="color: black; background-color: black; width:75%;" /> 
+        <div class="Continente"> 
+            <div class="Continente-mapa"> 
+                <h2>Americá</h2>
+                <img src="Imagenes/Continentes/América.png">
+            </div>
+            <div class="Continente-Banderas"> 
+                <div id="Bandera"> <a href="">
+                    <img src="Imagenes/Continente_Equipos/America/Argentina/Bandera Argentina.png">
+                    <p>Argentina</p></a>
+                </div>
+                <div id="Bandera"> <a href="">
+                    <img src="Imagenes/Continente_Equipos/America/Brazil/Bandera Brazil.png">
+                    <p>Brazil</p></a>
+                </div>
+                <div id="Bandera"> <a href="">
+                    <img src="Imagenes/Continente_Equipos/America/Canadá/Bandera Canadá.png">
+                    <p>Canadá</p></a>
+                </div>
+                <div id="Bandera"> <a href="">
+                    <img src="Imagenes/Continente_Equipos/America/Costa Rica/Bandera Costa Rica.png">
+                    <p>Costa Rica</p> </a>
+                </div>
+                <div id="Bandera"> <a href="">
+                    <img src="Imagenes/Continente_Equipos/America/Ecuador/Bandera Ecuador.png">
+                    <p>Ecuador</p> </a>
+                </div>
+                <div id="Bandera"> <a href="">
+                    <img src="Imagenes/Continente_Equipos/America/Estados Unidos/Bandera Estados Unidos.png">
+                    <p>Estados Unidos</p> </a>
+                </div>
+                <div id="Bandera"> <a href="">
+                    <img src="Imagenes/Continente_Equipos/America/México/Bandera México.png">
+                    <p>México</p> </a>
+                </div>
+                <div id="Bandera"> <a href="">
+                    <img src="Imagenes/Continente_Equipos/America/Uruguay/Bandera Uruguay.png">
+                    <p>Uruguay</p> </a>
+                </div>
+            </div>
+        </div>
+        <hr style="color: black; background-color: black; width:75%;" /> 
+        <div class="Continente"> 
+            <div class="Continente-mapa"> 
+                <h2>Asia</h2>
+                <img src="Imagenes/Continentes/Asia.png">
+            </div>
+            <div class="Continente-Banderas"> 
+                <div id="Bandera"> <a href="">
+                    <img src="Imagenes/Continente_Equipos/Asia/Arabia Saudita/Bandera Arabia Saudita.png">
+                    <p>Arabia Saudita</p></a>
+                </div>
+                <div id="Bandera"> <a href="">
+                    <img src="Imagenes/Continente_Equipos/Asia/Corea del Sur/Bandera Corea del Sur.png">
+                    <p>Corea del Sur</p></a>
+                </div>
+                <div id="Bandera"> <a href="">
+                    <img src="Imagenes/Continente_Equipos/Asia/Irán/Bandera Irán.png">
+                    <p>Irán</p></a>
+                </div>
+                <div id="Bandera"> <a href="">
+                    <img src="Imagenes/Continente_Equipos/Asia/Japón/Bandera Japón.png">
+                    <p>Japón</p> </a>
+                </div>
+                <div id="Bandera"> <a href="">
+                    <img src="Imagenes/Continente_Equipos/Asia/Qatar/Bandera Qatar.png">
+                    <p>Qatar</p> </a>
+                </div>
+            </div>
+        </div>
+        <hr style="color: black; background-color: black; width:75%;" /> 
+        <div class="Continente"> 
+            <div class="Continente-mapa"> 
+                <h2>Europa</h2>
+                <img src="Imagenes/Continentes/Europa.png">
+            </div>
+            <div class="Continente-Banderas"> 
+                <div id="Bandera"> <a href="">
+                    <img src="Imagenes/Continente_Equipos/Europa/Alemania/Bandera Alemania.png">
+                    <p>Alemania</p></a>
+                </div>
+                <div id="Bandera"> <a href="">
+                    <img src="Imagenes/Continente_Equipos/Europa/Bélgica/Bandera Bélgica.png">
+                    <p>Bélgica</p></a>
+                </div>
+                <div id="Bandera"> <a href="">
+                    <img src="Imagenes/Continente_Equipos/Europa/Croacia/Bandera Croacia.png">
+                    <p>Croacia</p></a>
+                </div>
+                <div id="Bandera"> <a href="">
+                    <img src="Imagenes/Continente_Equipos/Europa/Dinamarca/Bandera Dinamarca.png">
+                    <p>Dinamarca</p> </a>
+                </div>
+                <div id="Bandera"> <a href="">
+                    <img src="Imagenes/Continente_Equipos/Europa/España/Bandera España.png">
+                    <p>España</p> </a>
+                </div>
+                <div id="Bandera"> <a href="">
+                    <img src="Imagenes/Continente_Equipos/Europa/Francia/Bandera Francia.png">
+                    <p>Francia</p> </a>
+                </div>
+                <div id="Bandera"> <a href="">
+                    <img src="Imagenes/Continente_Equipos/Europa/Gales/Bandera Gales.png">
+                    <p>Gales</p> </a>
+                </div>
+                <div id="Bandera"> <a href="">
+                    <img src="Imagenes/Continente_Equipos/Europa/Inglaterra/Bandera Inglaterra.png">
+                    <p>Inglaterra</p> </a>
+                </div>
+                <div id="Bandera"> <a href="">
+                    <img src="Imagenes/Continente_Equipos/Europa/Países Bajos/Bandera Países Bajos.png">
+                    <p>Países Bajos</p> </a>
+                </div>
+                <div id="Bandera"> <a href="">
+                    <img src="Imagenes/Continente_Equipos/Europa/Polonia/Bandera Polonia.png">
+                    <p>Polonia</p> </a>
+                </div>
+                <div id="Bandera"> <a href="">
+                    <img src="Imagenes/Continente_Equipos/Europa/Portugal/Bandera Portugal.png">
+                    <p>Portugal</p> </a>
+                </div>
+                <div id="Bandera"> <a href="">
+                    <img src="Imagenes/Continente_Equipos/Europa/Serbia/Bandera Serbia.png">
+                    <p>Serbia</p> </a>
+                </div>
+            </div>
+        </div>
+        <hr style="color: black; background-color: black; width:75%;" />
+        <div class="Continente"> 
+            <div class="Continente-mapa"> 
+                <h2>Oceania</h2>
+                <img src="Imagenes/Continentes/Oceania.png">
+            </div>
+            <div class="Continente-Banderas"> 
+                <div id="Bandera"> <a href="">
+                    <img src="Imagenes/Continente_Equipos/Oceania/Australia/Bandera Australia.png">
+                    <p>Australia</p></a>
+                </div>
+            </div>
+        </div>
     </div>
 
     <footer>

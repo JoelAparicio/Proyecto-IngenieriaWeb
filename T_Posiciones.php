7e7668ea6814dac57b18297d7a949d29702f11f2
JoelAparicio <105009975+JoelAparicio@users.noreply.github.com>
2022-11-26 20:01:25 -0500

    Se coloco el header general.

--- T_Posiciones.php
+++ T_Posiciones.php
@@ -5,8 +5,29 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tabla de posiciones</title>
+    <link href="index.css" rel="stylesheet" type="text/css">
 </head>
 <body>
-    
+    <header>
+        <div>
+            <img src="Imagenes/Otros/logo.png" id="encabezado-logo">
+        </div>
+        <nav>
+            <ul  class="menu">
+                <li><a href="Inicio.php">Inicio</a></li>
+                <li><a href="T_Equipos.php">Equipos</a></li>
+                <li><a href="Resultados.php">Resultados</a></li>
+                <li><a href="T_Posiciones.php">Posiciones</a></li>
+                <li><a href="Favoritos.php">Favoritos</a></li>
+                <li>
+                    <img src="Imagenes/Otros/usuario.png" id="menu-usuario">
+                    <ul>
+                        <li><a href="">Registrar</a></li>
+                        <li><a href="">Iniciar sesión</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+    </header>
 </body>
 </html>

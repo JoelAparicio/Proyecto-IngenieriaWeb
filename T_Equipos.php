048f4e0353fd39180ce4bff42b4c63baf9ee0b52
Gabriela <105010227+GabriAizawa@users.noreply.github.com>
2022-11-26 19:59:55 -0500

    Primer Cambio

---
+++ T_Equipos.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Equipo</title>
+    <link href="index.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+    <header>
+        <!-- Aquí ira el encabezado de Aire  -->
+
+    </header>
+
+    <div> 
+       <h2>Equipos del Mundial</h2>
+    </div>
+
+    <div>
+
+    
+    </div>
+
+    <footer>
+        <!-- Aquí ira el pie de página de Aire  -->
+    </footer>   
+</body>
+</html>

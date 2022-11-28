be98fc228dba26b76b5471f7372f753d09afbcbd
Gabriela <105010227+GabriAizawa@users.noreply.github.com>
2022-11-28 18:23:28 -0500

    Commit Añadiendo
    Resultados pagina incompleta

--- Resultados.php
+++ Resultados.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -29,5 +30,89 @@
             </ul>
         </nav>
     </header>
+
+    <div> 
+        <div>
+            <h1>Partidos</h1>
+        </div>
+
+        <div class="fecha">
+        <form method="POST" name= "formulario fecha">
+            <div class=fecha_1>
+                <label for="fecha">Día:</label>
+                    <select class="seleccionar" name = "day" id="day">
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                        <option value="6">6</option>
+                        <option value="7">7</option>
+                        <option value="8">9</option>
+                        <option value="10">10</option>
+                        <option value="11">11</option>
+                        <option value="12">12</option>
+                        <option value="13">13</option>
+                        <option value="14">14</option>
+                        <option value="15">15</option>
+                        <option value="16">16</option>
+                        <option value="17">17</option>
+                        <option value="18">18</option>
+                        <option value="19">19</option>
+                        <option value="20">20</option>
+                        <option value="21">21</option>
+                        <option value="22">22</option>
+                        <option value="23">23</option>
+                        <option value="24">24</option>
+                        <option value="25">25</option>
+                        <option value="26">26</option>
+                        <option value="27">27</option>
+                        <option value="28">28</option>
+                        <option value="29">29</option>
+                        <option value="30">30</option>
+                        <option value="31">31</option>
+                    </select><br><br>
+            </div>    
+            <div class=fecha_1>
+                <label for="month">Month:</label>
+                    <select class="seleccionar" id="month" name="month">
+                        <option>November</option>
+                        <option>December</option>
+                    </select> <br><br>
+            </div>
+
+            <INPUT TYPE="submit" name="Enviar">
+
+        </form>
+        </div>
+
+        <div>
+            <h1>Fichas</h1>
+        </div>
+
+    </div>
+
 </body>
 </html>
+
+<?php
+    $day = $_POST['day'];
+    $month = $_POST['month'];
+    
+    if($day == 20 && $month =="November"){
+        echo " Fecha: $day, $month <br>";
+        echo '<img src="Imagenes/Continente_Equipos/Africa/Camerún/Bandera Camerún.png">';
+    }
+
+    elseif($day <=19 && $month == "November"){
+        echo "No a iniciado la Copa Mundial";
+    }
+    elseif($day >= 19 && $month == "December"){
+        echo "Partidos terminados de la Copa Mundial";
+    }
+
+
+
+?>
+
+        

b4ee7b561d86637f592e66afcb43f9ca7db76af6
JoelAparicio <105009975+JoelAparicio@users.noreply.github.com>
2022-11-29 08:49:01 -0500

    realizacion de cookies

--- Registro_code.php
+++ Registro_code.php
@@ -4,16 +4,16 @@ if(isset($_POST['registrar'])){
     $usuario = trim($_POST['usuario-registro']);
     $correo = trim($_POST['correo']);
     $password = trim($_POST['password-registro']);
-    $count = 0;
+    $count = 1;
     $sql_registro = "INSERT INTO usuarios (usuario,contraseña,correo, rol) VALUES ('$usuario', '$password', '$correo', 'Usuario')";
     $resultado = mysqli_query($conn, $sql_registro);
-
+    setcookie('VecesRegistros', $count, time() + 86700);
     if($resultado){
         ?>
         <h3 class="registro-exito">¡Te has registrado correctamente!</h3>
         <?php
-            $count++;
-            setcookie('Veces Registros', $count, time() + 86700);
+            $existente = @$_COOKIE['VecesRegistros'];
+            setcookie('VecesRegistros', $existente + 1, time() + 86700);
             mysqli_close($conn);
     }
     else{
